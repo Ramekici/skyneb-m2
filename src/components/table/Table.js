a9ca3078cdd6c7bc665974a15fafe76fc6b2e80a
Ramekici <46330945+Ramekici@users.noreply.github.com>
2021-04-04 14:51:22 +0300

    last-new

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './Table.scss';
 import Emir from './Emir/Emir';
-import {dataTables} from '../datas/dataTable';
-import {dataTableBelow} from '../datas/dataTableLabel';
+import { dataTables } from '../datas/dataTable';
+import { dataTableBelow } from '../datas/dataTableLabel';
 import Button from '../common/Button';
+import ArrowDotDown from '../svg/ArrowDotDown';
+
+
 
 export default function Table() {
 
     return (
         <div className="table">
-            <div className="table-row">
+            <div className="table-row" style={{paddingRight:"0.5rem"}}>
                 <div className="table-nav-item">
-                   <Button name="İşlemler(Pozisyonlar)" className="active"/>
-                   <Button name="Risk Analizi & İşlem Analizi" />
-                   <Button name="Hesap Geçmişi" />
+                    <Button name="İşlemler(Pozisyonlar)" className="active" />
+                    <Button name="Risk Analizi & İşlem Analizi" />
+                    <Button name="Hesap Geçmişi" />
                 </div>
+                <ArrowDotDown />
             </div>
             <div className="table-row">
                 {dataTableBelow.map(item =>
